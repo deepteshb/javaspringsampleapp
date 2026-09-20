c9b7c533362ccbdbd31bf9b55f5240aa7b7395b9
“MacDee” <deepteshdevops@outlook.com>
2026-09-20 22:32:22 +1000

    adding initial application code

---
+++ javaspringbootsampleapp/src/main/java/com/example/javaspringbootsampleapp/JavaspringbootsampleappApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaspringbootsampleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaspringbootsampleappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaspringbootsampleappApplication.class, args);
+	}
+
+}
---
+++ javaspringbootsampleapp/src/test/java/com/example/javaspringbootsampleapp/JavaspringbootsampleappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaspringbootsampleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaspringbootsampleappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
